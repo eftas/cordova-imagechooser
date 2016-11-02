cd3fcb52001c412cae74d162ec2437e4c5e3a870
Obinna Agba <obinna@mobnia.com>
2016-11-02 15:11:18 +0100

    fixed some forgotten imports

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -11,6 +11,7 @@ import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaResourceApi;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONObject;
